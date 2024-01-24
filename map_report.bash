cea083dfe181d1fa2055214a4610c07ad5d7b802
AdRi1t <adrien.taberner@gmail.com>
2024-01-24 17:01:21 +0100

    seconde version pour OpenMP et script pour linearo map

---
+++ map_report.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=perfReport
+#SBATCH --output=perfReport_%J.o
+#SBATCH --error=perfReport_%J.e
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=01:00:00
+#SBATCH --export=ALL
+
+echo ${SLURM_SUBMIT_DIR}
+cd ${SLURM_SUBMIT_DIR}
+
+# Run your script
+map --profile -openmp-threads=1 -o "${SLURM_JOB_NAME}_${SLURM_JOB_ID}" ./"$@"
+
