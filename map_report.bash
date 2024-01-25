8792c3d2858bbce67c4e44f78fdaf33f6977c216
AdRi1t <adrien.taberner@gmail.com>
2024-01-25 14:16:30 +0100

    Update map_report.bash

--- map_report.bash
+++ map_report.bash
@@ -4,7 +4,7 @@
 #SBATCH --error=slurm/map_report_%J.e
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=64
 #SBATCH --time=01:00:00
 #SBATCH --export=ALL
 
@@ -12,5 +12,4 @@ echo ${SLURM_SUBMIT_DIR}
 cd ${SLURM_SUBMIT_DIR}
 
 # Run your script
-map --profile -openmp-threads=1 -o "${SLURM_JOB_NAME}_${SLURM_JOB_ID}" ./"$@"
-
+map --profile -openmp-threads=64 -o "${SLURM_JOB_NAME}_${SLURM_JOB_ID}" ./"$@"
